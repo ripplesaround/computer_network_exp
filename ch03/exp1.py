c4d5719f39850096cff6938823b0e40def6b74df
Fan Wenxiao <ripplesaround@sina.com>
2020-03-22 21:45:45 +0800

    大三下计算机网络实验

--- ch03/exp1.py
+++ ch03/exp1.py
@@ -1,5 +1,24 @@
 import numpy as np
 
 class CRC:
-    def __init__(self):
-        self.K = 1
+    def __init__(self,k=25):
+        # temp = np.random.randint(2, size=k)
+        # self.InfoString1 = '0110'
+        # for i in temp:
+        #     self.InfoString1 =  self.InfoString1 + str(i)
+        # self.InfoString1 =  self.InfoString1 + "110"
+        self.InfoString1 = '101001'
+        self.P = '1100'
+
+    def cal(self):
+        len_infostr_orgin = len(self.InfoString1)
+        for i in range(len(self.P)-1):
+            self.InfoString1 += '0'
+        # 应该先计算过去一次
+        # for i in range(len_infostr_orgin):
+        #     temp = self.InfoString1[i:i+len(self.P)]
+
+
+
+test = CRC()
+test.cal()
