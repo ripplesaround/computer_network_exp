d660c3ace057efaabe8b16eb764bc2e773c9df8a
Fan Wenxiao <ripplesaround@sina.com>
2020-03-26 19:35:02 +0800

    学习总是痛苦的，也是快乐的

---
+++ ch03/实验4/hostA.py
@@ -0,0 +1,117 @@
+# coding=utf-8
+
+'''
+Author: ripples
+Email: ripplesaround@sina.com
+
+date: 2020/3/25 15:01
+desc:
+'''
+# 发送端
+import socket
+import sys
+# 调用实验1 中的CRC
+sys.path.append('../实验1/python')
+from CRC import *
+import time
+import _thread
+import threading
+
+UDPPort=8888
+FilterError=10
+FilterLost=10
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+f = open('config.txt', 'r')
+messages = f.read().split('\n')
+messages = [message for message in messages if message != '']
+test = CRC()
+
+
+already_send = 0
+finish = len(messages)
+re = ''
+flag_ack = False
+
+class rec_ack:
+    def __init__(self):
+        self._running = True
+
+    def terminate(self):
+        self._running = False
+
+    def run(self,n):
+        global flag_ack
+        flag_ack = False
+        global re
+        while self._running :
+            re = client.recvfrom(1024)
+            if re[0].decode('utf-8')!=None:
+                flag_ack = True
+                break
+
+se = 'start'.encode('utf-8')
+client.sendto(se, ('127.0.0.1', UDPPort))
+re = client.recvfrom(1024)
+print(f"来自{re[1]}的消息:{re[0].decode('utf-8')}")
+selfport = int(re[0].decode('utf-8'))
+re = ''
+flag_re = False
+print("开始发送")
+print("----------------------------------------")
+
+next_frame_to_send = 0
+
+while 1:
+    # todo 序号需要修改
+    # seq放在第一位
+    seq = next_frame_to_send
+    se = str(seq)+ test.send_cal(messages[already_send])
+    se = se.encode('utf-8')
+    if np.random.randint(0, FilterLost) > 0:
+        client.sendto(se, ('127.0.0.1', UDPPort))
+        print('已经发送第', already_send, "条数据")
+    else:
+        print('未发送第', already_send, "条数据")
+
+    # done 计时器
+
+    c = rec_ack()
+    t = threading.Thread(target=c.run, args=(10,))
+    t.start()
+    current_time = time.time()
+    while(1):
+        now = time.time()
+        if flag_ack:
+            # print("接收ack")
+            break
+        if now - current_time > 2:
+            print("time out")
+            client.sendto('timeout'.encode('utf-8'), ('127.0.0.1',selfport))
+            flag_re = True
+            break
+    c.terminate()
+    t.join()
+    if flag_re :
+        flag_re = False
+        print('重新发送第', already_send, "条数据，原因：帧丢失")
+        continue
+    ack = int(re[0].decode('utf-8'))
+    if ack == next_frame_to_send:
+        print('成功发送第', already_send, "条数据")
+        print(f"接收到确认帧，发送回确认帧的ack为{ack}")
+        next_frame_to_send += 1
+        next_frame_to_send %= 2
+        print("----------------")
+        already_send += 1
+    else:
+        print('重新发送第', already_send, "条数据，原因：帧出错")
+        continue
+
+    if finish == already_send:
+        client.sendto('stop'.encode('utf-8'), ('127.0.0.1', UDPPort))
+        break
+
+print("----------------------------------------")
+print("发送结束")
---
+++ ch03/实验4/hostB.py
@@ -0,0 +1,75 @@
+# coding=utf-8
+
+'''
+Author: ripples
+Email: ripplesaround@sina.com
+
+date: 2020/3/25 15:01
+desc:
+'''
+
+# 接收端
+import socket
+import sys
+# 调用实验1 中的CRC
+sys.path.append('../实验1/python')
+from CRC import *
+import time
+import numpy as np
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+UDPPort=8888
+server.bind(('127.0.0.1',UDPPort))
+test = CRC()
+FilterError=10
+FilterLost=10
+
+from_client = server.recvfrom(1024)
+print(f"来自{from_client[1]}的消息：{from_client[0].decode('utf-8')}")
+server.sendto(str(from_client[1][1]).encode('utf-8'),from_client[1])
+
+test1 = 0
+print("开始接收")
+print("----------------------------------------")
+
+frame_expected = 0
+while 1:
+    from_client_data = server.recvfrom(1024)
+    if from_client_data[0].decode('utf-8') =='stop':
+        break
+    seq = int(from_client_data[0].decode('utf-8')[0])
+    mess = from_client_data[0].decode('utf-8')[1:]
+    # 帧出错
+    if np.random.randint(0,FilterError)<1:
+        s1 = list(mess)
+        s1[10] = str((int(s1[10])+1)%2)
+        mess = ''.join(s1)
+    re = test.rec_cal(mess)
+
+    if re =='error':
+        ack = (seq+1)%2
+        se = str(ack).encode('utf-8')
+        server.sendto(se, from_client_data[1])
+        print(f"确认帧的序号{ack}")
+        test1 += 1
+        print("此次传输中帧损坏")
+        print('----------------')
+        continue
+    print(f"来自{from_client_data[1]}的消息：{re}，seq为{seq}")
+
+    if seq == frame_expected:
+        # pass it to network layer
+        frame_expected += 1
+        frame_expected %= 2
+    ack = 1-frame_expected
+
+    se = str(ack).encode('utf-8')  # 其实可以没有内容
+    if np.random.randint(0,FilterLost)>0:
+        server.sendto(se, from_client_data[1])  # 可以直接实现阻塞的功能
+        print(f"确认帧的序号{ack}")
+        print('----------------')
+    # server.sendto(se, from_client_data[1])
+    test1 += 1
+
+print("----------------------------------------")
+print("接收结束")
