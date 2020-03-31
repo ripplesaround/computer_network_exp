fc2a54644352052692f362e0ab5374d7a3e749e2
Fan Wenxiao <ripplesaround@sina.com>
2020-03-31 14:56:03 +0800

    学习总是痛苦的，也是快乐的

--- ch03/实验4/python/hostA.py
+++ ch03/实验4/python/hostA.py
@@ -65,7 +65,7 @@ for i in range(MAX_SEQ+1):
 re = ''
 
 def send():
-    s = frame(test.send_cal(buffer[next_frame_to_send]),next_frame_to_send,frame_expected)
+    s = frame(test.send_cal(buffer[next_frame_to_send]),next_frame_to_send,(frame_expected+MAX_SEQ)%(MAX_SEQ+1))
     host.sendto(json.dumps(obj=s.__dict__, ensure_ascii=False).encode('utf-8'), ('127.0.0.1', tarPort))
 
 flag_control = 0        #相当于switch case
@@ -83,6 +83,7 @@ def wait_for_event():
 
 def from_network_layer():
     # 从message中取数组，模拟从网络层取包
+    global into_buffer
     buffer[next_frame_to_send] = messages[into_buffer]
 
 k = host.recvfrom(1024)
@@ -96,46 +97,82 @@ def rec(arg):
     global re
     global flag_control
     re = host.recvfrom(1024)
+
     flag_control = 2
 
 
 
 def wait_for_event():
+    global re
+    global flag_control
+    orgin = flag_control
+    re = ''
     t1 = threading.Thread(target=rec, args=(3,))
     t1.start()
     t1.join(0.01)
+    if re == '':
+        host.sendto('run'.encode('utf-8'), ('127.0.0.1', selfPort))
+        t1.join(0.00001)
+        print("此时未接收到frame")
+        flag_control = orgin
     print("flag_control",flag_control)
 
 
 enable_network_layer()
-
+already_send = 0
+a = 0
+# for a in range(60):
 while 1:
     wait_for_event()
 
     if flag_control == 1:
         print("发送一帧")
+        if into_buffer == len(messages):
+            host.sendto('stop'.encode('utf-8'), ('127.0.0.1', tarPort))
+            print("发送结束")
+            break
         from_network_layer()
         into_buffer += 1
         nbuffered += 1
         send()
-        next_frame_to_send += 1
-        next_frame_to_send %= MAX_SEQ
+        if (next_frame_to_send < MAX_SEQ):
+            next_frame_to_send += 1
+        else:
+            next_frame_to_send = 0
+        # next_frame_to_send += 1
+        # next_frame_to_send %= MAX_SEQ
     elif flag_control == 2:
-        print("收到一帧")
+        if re[0].decode('utf-8') == 'run':
+            continue
+        if re[0].decode('utf-8') == 'stop':
+            print("接收结束")
+            break
+        print(f"收到一帧,frame_expected:{frame_expected},ack_expend:{ack_expend},next_frame_to_send:{next_frame_to_send},nbuffer:{nbuffered}")
         # 将json转换成frame
         json_str = re[0].decode('utf-8')
         s = json.loads(json_str, object_hook=dict2frame)
-        print(f"来自{re[1]}的消息：{s.info}")
+        print(f"来自{re[1]}的消息：{s.info},s.seq:{s.seq},s.ack:{s.ack}")
+        re = ''
 
         if(s.seq == frame_expected):
             to_network_layer()
-            frame_expected += 1
-            frame_expected %= MAX_SEQ
+            if (frame_expected < MAX_SEQ):
+                frame_expected += 1
+            else:
+                frame_expected = 0
+            # frame_expected += 1
+            # frame_expected %= MAX_SEQ
         while(between(ack_expend,s.ack,next_frame_to_send)):
             nbuffered -= 1
             # todo stop_time
-            ack_expend += 1
-            ack_expend %= MAX_SEQ
+            print(f"确认第{ack_expend}帧已送达")
+            already_send += 1
+            if (ack_expend < MAX_SEQ):
+                ack_expend += 1
+            else:
+                ack_expend = 0
+            # ack_expend += 1
+            # ack_expend %= MAX_SEQ
     elif flag_control == 3:
         print("check sum error")
     elif flag_control == 4:
@@ -143,11 +180,19 @@ while 1:
         next_frame_to_send = ack_expend
         for i in range(nbuffered):
             send()
-            next_frame_to_send += 1
-            next_frame_to_send %= MAX_SEQ
+            if (next_frame_to_send < MAX_SEQ):
+                next_frame_to_send += 1
+            else:
+                next_frame_to_send = 0
+            # next_frame_to_send += 1
+            # next_frame_to_send %= MAX_SEQ
 
     if nbuffered < MAX_SEQ:
         enable_network_layer()
     else:
         disable_network_layer()
-
+    print("-------------------")
+    print(a)
+print("end")
+print("into_buffer",into_buffer)
+print("already_send",already_send)
--- ch03/实验4/python/hostB.py
+++ ch03/实验4/python/hostB.py
@@ -66,7 +66,7 @@ def dict2frame(d):
 
 
 def send():
-    s = frame(test.send_cal(buffer[next_frame_to_send]),next_frame_to_send,frame_expected)
+    s = frame(test.send_cal(buffer[next_frame_to_send]),next_frame_to_send,(frame_expected+MAX_SEQ)%(MAX_SEQ+1))
     host.sendto(json.dumps(obj=s.__dict__, ensure_ascii=False).encode('utf-8'), ('127.0.0.1', tarPort))
 
 flag_control = 0
@@ -98,41 +98,76 @@ def rec(arg):
     flag_control = 2
 
 def wait_for_event():
+    global re
+    global flag_control
+    orgin = flag_control
+    re = ''
     t1 = threading.Thread(target=rec, args=(3,))
     t1.start()
     t1.join(0.01)
+    if re == '':
+        host.sendto('run'.encode('utf-8'), ('127.0.0.1', selfPort))
+        t1.join(0.00001)
+        print("此时未接收到frame")
+        flag_control = orgin
     print("flag_control",flag_control)
 
 
 enable_network_layer()
-
+already_send = 0
+a = 0
+# for a in range(60):
 while 1:
     wait_for_event()
     if flag_control ==1:
         print("发送一帧")
+        if into_buffer == len(messages):
+            host.sendto('stop'.encode('utf-8'), ('127.0.0.1', tarPort))
+            print("发送结束")
+            break
         from_network_layer()
         into_buffer += 1
         nbuffered += 1
         send()
-        next_frame_to_send += 1
-        next_frame_to_send %= MAX_SEQ
+        if (next_frame_to_send < MAX_SEQ):
+            next_frame_to_send += 1
+        else:
+            next_frame_to_send = 0
+        # next_frame_to_send += 1
+        # next_frame_to_send %= MAX_SEQ
     elif flag_control == 2:
-        print("收到一帧")
+        if re[0].decode('utf-8') =='run':
+            continue
+        if re[0].decode('utf-8') == 'stop':
+            print("接收结束")
+            break
+        print(f"收到一帧,frame_expected:{frame_expected},ack_expend:{ack_expend},next_frame_to_send:{next_frame_to_send},nbuffer:{nbuffered}")
         # 将json转换成frame
         json_str = re[0].decode('utf-8')
         s = json.loads(json_str, object_hook=dict2frame)
-        print(f"来自{re[1]}的消息：{s.info}")
+        print(f"来自{re[1]}的消息：{s.info},s.seq:{s.seq},s.ack:{s.ack}")
+        re = ''
 
         if (s.seq == frame_expected):
             to_network_layer()
-            frame_expected += 1
-            frame_expected %= MAX_SEQ
+            # frame_expected += 1
+            # frame_expected %= MAX_SEQ
+            if(frame_expected<MAX_SEQ):
+                frame_expected+=1
+            else:
+                frame_expected = 0
 
         while (between(ack_expend, s.ack, next_frame_to_send)):
             nbuffered -= 1
             # todo stop_time
-            ack_expend += 1
-            ack_expend %= MAX_SEQ
+            print(f"确认第{ack_expend}帧已送达")
+            already_send +=1
+            # ack_expend += 1
+            # ack_expend %= MAX_SEQ
+            if (ack_expend < MAX_SEQ):
+                ack_expend += 1
+            else:
+                ack_expend = 0
     elif flag_control == 3:
         print("check sum error")
     elif flag_control == 4:
@@ -140,11 +175,21 @@ while 1:
         next_frame_to_send = ack_expend
         for i in range(nbuffered):
             send()
-            next_frame_to_send += 1
-            next_frame_to_send %= MAX_SEQ
+            # next_frame_to_send += 1
+            # next_frame_to_send %= MAX_SEQ
+            if (next_frame_to_send < MAX_SEQ):
+                next_frame_to_send += 1
+            else:
+                next_frame_to_send = 0
 
     if nbuffered < MAX_SEQ:
         enable_network_layer()
     else:
         disable_network_layer()
+    print("-------------------")
+    print(a)
+print("end")
+print("into_buffer",into_buffer)
+print(len(messages))
+print("already_send",already_send)
 
