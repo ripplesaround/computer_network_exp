2034d9c867ac30820d4fd91f9d3232c6d3aa762f
Fan Wenxiao <ripplesaround@sina.com>
2020-04-09 21:31:10 +0800

    学习总是痛苦的，也是快乐的，有时还是迷惑的

--- ch04/实验1/python/main.py
+++ ch04/实验1/python/main.py
@@ -2,16 +2,49 @@ from winpcapy import WinPcapDevices
 from winpcapy import WinPcapUtils
 
 import dpkt
+from dpkt.compat import compat_ord
 import time
 import datetime
-
+print("---------------------------------")
+print("当前的网络配置器列表")
 list_device = WinPcapDevices.list_devices()
-print(list_device)
+for i,device in enumerate(list_device):
+    print(i,device)
 
+
+print("---------------------------------")
+print("选择捕获适配器的编号")
+flag = int(input(">>>"))
+print(f"正在捕获{flag}号网络适配器")
+print("---------------------------------")
+
+def mac_addr(address):
+    """Convert a MAC address to a readable/printable string
+
+       Args:
+           address (str): a MAC address in hex form (e.g. '\x01\x02\x03\x04\x05\x06')
+       Returns:
+           str: Printable/readable MAC address
+    """
+    return ':'.join('%02x' % compat_ord(b) for b in address)
 
 def packet_callback(win_pcap, param, header, pkt_data):
+    print("---------分析数据包----------")
+    print("------ethernet packet-------")
     eth = dpkt.ethernet.Ethernet(pkt_data)
-    # # 判断是否为IP数据报
+
+    print("the source address of the ethernet packet:",mac_addr(eth.src))
+    print("the destination address of the ethernet packet:", mac_addr(eth.dst))
+    # more types see http://www.iana.org/assignments/ethernet-numbers/ethernet-numbers.xhtml
+    if eth.type ==2048: # 16进制：0x0800
+        print("the Ethernet type:",eth.type,'means: IPv4')
+    elif eth.type == 34525: #0x86DD
+        print("the Ethernet type:",eth.type, 'means: IPv6')
+
+
+    print("----------IP packet---------")
+
+    # 判断是否为IP数据报
     if not isinstance(eth.data, dpkt.ip.IP):
         print("Non IP packet type not supported ", eth.data.__class__.__name__)
         return
@@ -32,5 +65,9 @@ def packet_callback(win_pcap, param, header, pkt_data):
     print(output2)
     print(output3)
     print(output4)
+    print("----------------------------\n")
 
-WinPcapUtils.capture_on(pattern="Realtek PCIe GBE Family Controller", callback=packet_callback)
+for i,key in enumerate(list_device.keys()):
+    if i== flag:
+        WinPcapUtils.capture_on_device_name(key, callback=packet_callback)
+        break
